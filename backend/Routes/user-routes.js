ba5d0a5ac3abc4187e5a99da39ff5a3a68428617
Apple <apple@Apples-MacBook-Pro.local>
2023-02-12 16:27:27 +0530

    login route working successfully, but problems with /allUsers

--- backend/Routes/user-routes.js
+++ backend/Routes/user-routes.js
@@ -6,7 +6,7 @@ var jwt = require('jsonwebtoken');
 const User = require('../Models/user')
 const getDb = require('../util/database').getDb;
 
-
+// creating user for signup 
 router.post('/createUser',[
     // Validation bty expresss Validator
     body('username','Enter a valid name').isLength({ min: 3 }),
@@ -59,15 +59,56 @@ catch (error) {
 
 
 
-router.get('/user',(req,res,next) => {
-    console.log('GET request for user');
-    res.json({"message": "/user ....It works"});
+router.post('/loginUser',[
+  body('email','Enter a valid email').isEmail(),
+  body('password','Enter a valid password').isLength({min:5})
+],async (req,res,next) => {
+    try {
+      const error = validationResult(req);
+      if (!error.isEmpty()){
+        return res.status(400).json({error:error,message:'Bad Request'});
+      }
+      const _db = getDb();
+      const {email,password} = req.body;
+      const _user = await _db.collection('users').findOne({email});
+      // console.log(_user);
+      if (!_user){
+        return res.status(400).json({message:'Please login with valid credentials.'});
+      }
+      const bool = await bcrypt.compare(password,_user.password);
+      if (!bool){
+        return res.status(400).json({message:'Please login with valid credentials'});
+      }
+      res.status(200).json({user:_user,message:"Welcome to Meet_UP"})
+    }
+    catch(error){
+      
+      res.status(500).json({message:'Some error occured'});
+    }
+
+    
 });
 
-router.post('/signup',(req,res,next) => {
-    console.log('POST request for signup');
-    res.json({"message": "It works"});
-});
+// Getting all users
+/*
+router.get('/allUsers',
+async(req,res)=>{
+  try{
+    const _db = getDb();
+    var users = _db.collection('users').find({}).toArray(function (err, result) {
+      if (err) {
+          res.send(err);
+      } else {
 
-
+          res.send(result);
+      }
+  })
+    res.status(200).json({users});
+  }
+  catch(error){
+    res.status(500).send("Some error ocuured");
+    console.log(error)
+  }
+})
+*/
 module.exports = router;
