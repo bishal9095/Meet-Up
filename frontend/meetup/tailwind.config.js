22a122f6eb9ba78de8818ff08e5822c68ef592f1
bishal9095 <bishal.ece319095@bppimt.ac.in>
2023-02-22 22:17:36 +0530

    FrontEnd Working

---
+++ frontend/meetup/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./Frontend/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
