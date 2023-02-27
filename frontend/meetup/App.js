660758f88abffbc29432a448033f36d63e6360a5
bishal9095 <bishal.ece319095@bppimt.ac.in>
2023-02-27 13:57:49 +0530

    Running FrontEnd

--- frontend/meetup/App.js
+++ frontend/meetup/App.js
@@ -2,22 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
 import { Text, View } from 'react-native';
 
+import StackNavigator from './StackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+
 export default function App() {
   return (
-    // <View style={styles.container}>
-      <View className="flex-1 items-center justify-center bg-[#10b981]">
-      <Text>Awesome</Text>
-      <Text>So this is how i can make apps</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- frontend/meetup/Screens/HomeScreen.js
+++ frontend/meetup/Screens/HomeScreen.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    // <View style={styles.container}>
+      <View className="flex-1 items-center justify-center bg-[#10b981]">
+      <Text>Awesome</Text>
+      <Text>So this is how i can make apps</Text>
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 export default HomeScreen
